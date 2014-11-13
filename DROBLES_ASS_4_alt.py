5004e2106d518d5ad793c3a4999fbb826c4789ca
Daniel Robles <daniel@danieljrobles.com>
2014-11-13 14:27:05 -0800

    Formatting

--- DROBLES_ASS_4_alt.py
+++
@@ -1,3 +0,0 @@
-number = 12345
-for x in range(1,6):
-    print (12345 % 10^x)
--- api.py
+++
@@ -1,3 +0,0 @@
-import urllib.request
-r = urllib.request.urlopen("http://sports.espn.go.com/nfl/bottomline/scores").read()
-print(type(r.split('&')))
